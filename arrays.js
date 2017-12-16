2c6b416b31b3206c99c79d83168c32224f144a4d
David Mendelovits <david.mendelovits@gmail.com>
2017-12-16 03:25:47 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray() {
+  var array = [1]
+  return ['foo', ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array) {
+    array.unshift('foo');
+    return array
+}
+
+function addElementToEndOfArray() {
+  var array = [1]
+  return [...array, 'foo']
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push('foo')
+  return array
+}
